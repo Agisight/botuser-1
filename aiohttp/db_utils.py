48b1c2492eeb29ee858d0c3936fb80ff92ac36da
Petrucci89 <petrucci@ua.fm>
2019-06-28 20:52:24 +0300

    bug f

--- aiohttp/db_utils.py
+++ aiohttp/db_utils.py
@@ -50,38 +50,6 @@ async def update_user(bot_id, chat_id, **kwargs):
         return await conn.fetch(sql_query, *vars)
 
 
-async def get_user(bot_id, chat_id):
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT * FROM bot_botuser WHERE bot_id = $1 AND chat_id = $2', bot_id, chat_id)
-        if result:
-            return result[0]
-        return result
-
-
-async def get_refcount(chat_id):
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT count(*) FROM bot_botuser WHERE parent = %s' % chat_id)
-        return result[0]["count"]
-
-
-async def get_user_by_id(id):
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT * FROM bot_botuser WHERE id = %s' % id)
-        if not result:
-            return []
-        else:
-            return result[0]
-
-
-async def get_kurs():
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT * FROM bot_settings')
-        if not result:
-            return []
-        else:
-            return result[0]["kurs"]
-
-
 async def get_settings():
     async with pool.acquire() as conn:
         result = await conn.fetch('SELECT * FROM bot_settings LIMIT 1')
@@ -91,146 +59,13 @@ async def get_settings():
             return result[0]
 
 
-async def get_count_users():
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT count(*) FROM bot_botuser')
-        if not result:
-            return []
-        else:
-            return result[0]["count"]
-
-
-async def get_summa():
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT sum(balance) FROM bot_botuser')
-        if not result:
-            return []
-        else:
-            return result[0]["sum"]
-
-
-async def get_count_today_users(date):
-    async with pool.acquire() as conn:
-        result = await conn.fetch("SELECT count(*) FROM bot_botuser where DATE(date_in + interval '3 hours') = '%s'" % date)
-        if not result:
-            return []
-        else:
-            return result[0]["count"]
-
-
-async def get_users(offset, limit):
-    print('get_users %s %s' % (offset, limit))
-
-    with open("error_compaign.txt", 'a+') as file:
-        file.write('get_users %s %s\n' % (offset, limit))
-
-    async with pool.acquire() as conn:
-        return await conn.fetch("SELECT * FROM bot_botuser WHERE otpiska = FALSE ORDER BY id ASC OFFSET %s LIMIT %s" % (offset, limit))
-        #return await conn.fetch("SELECT * FROM bot_botuser ORDER BY id ASC OFFSET %s LIMIT %s" % (offset, limit))
-
-
-async def get_users_stat():
-    res = []
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT count(*) FROM bot_botuser WHERE otpiska = FALSE')
-        res.append(result[0]["count"])
-        result = await conn.fetch('SELECT count(*) FROM bot_botuser WHERE otpiska = TRUE')
-        res.append(result[0]["count"])
-        return res
-
-
-async def get_compaign(id):
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT * FROM bot_compaign WHERE id = %s' % id)
-        if not result:
-            return []
-        else:
-            return result[0]
-
-
-async def set_user_otpiska(chat_id):
-    print('set_user_otpiska')
-    async with pool.acquire() as conn:
-        await conn.fetch("UPDATE bot_botuser SET otpiska = TRUE WHERE chat_id = %s" % chat_id)
-
-
-
-async def new_compaign():
-    print('new_compaign')
-    async with pool.acquire() as conn:
-        r = await conn.fetch("SELECT * FROM bot_compaign WHERE is_new = 1 limit 1")
-        print(r)
-        return r
-
-
-async def set_old_compaign(id):
-    print('set_old_compaign')
-    async with pool.acquire() as conn:
-        await conn.fetch("UPDATE bot_compaign SET is_new = 0 WHERE id = %s" % id)
-
-
-async def add_history(user_id, history_type, summa, summa_crystal, payment_method, wallet, date_in, is_done, log=None):
-    async with pool.acquire() as conn:
-        query = "INSERT INTO bot_history (user_id, history_type, summa, summa_crystal, payment_method, wallet, date_in, is_done, log) VALUES ($1::bigint, $2::varchar, $3::real, $4::real, $5::varchar, $6::varchar, $7::timestamp, $8::boolean, $9::varchar) RETURNING *"
-        await conn.fetch(query, user_id, history_type, summa, summa_crystal, payment_method, wallet, date_in, is_done, log)
-
-
-async def get_history(user_id):
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT * FROM bot_history WHERE user_id = %s AND is_done = TRUE' % user_id)
-        if not result:
-            return []
-        else:
-            return result
-
-
-async def add_addition(user_id, summa, date_in):
-    async with pool.acquire() as conn:
-        query = "INSERT INTO bot_addition " \
-                "(user_id, summa, date_in) " \
-                "VALUES ($1::bigint, $2::real, $3"
-        await conn.fetch(query, user_id, summa, date_in)
-
-
-async def get_sum_additions():
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT sum(summa) FROM bot_addition')
-        if not result:
-            return 0
-        else:
-            return result[0]["sum"]
-
-
-async def get_today_sum_additions(d):
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT sum(summa) FROM bot_addition WHERE date_in = $1', d)
-        if not result:
-            return 0
-        else:
-            return result[0]["sum"]
-
-
-async def get_last_trades():
-    async with pool.acquire() as conn:
-        return await conn.fetch('SELECT * FROM bot_trade ORDER BY id DESC LIMIT 3')
-
-
-async def get_margin(obmen_type):
-    async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT * FROM bot_margin WHERE obmen_type = $1', obmen_type)
-        if result:
-            return result[0]
-        else:
-            return []
-
-
 async def create_order(user_id, out_payment, in_payment, out_amount, in_amount, out_wallet, in_wallet, status, delta):
     async with pool.acquire() as conn:
 
         d = datetime.today()
         d_cancel = d + delta
 
-        query = "INSERT INTO bot_order " \
+        query = "INSERT INTO cabinet_order " \
                 "(user_id, out_payment, in_payment, out_amount, in_amount, out_wallet, in_wallet, status, date_in, date_cancel) " \
                 "VALUES " \
                 "($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) " \
@@ -245,7 +80,7 @@ async def create_order(user_id, out_payment, in_payment, out_amount, in_amount, 
 
 async def get_order(order_id):
     async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT * FROM bot_order WHERE id = $1', order_id)
+        result = await conn.fetch('SELECT * FROM cabinet_order WHERE id = $1', order_id)
         if result:
             return result[0]
         else:
@@ -254,7 +89,7 @@ async def get_order(order_id):
 
 async def get_order_by_wallet(out_wallet):
     async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT * FROM bot_order WHERE out_wallet = $1', out_wallet)
+        result = await conn.fetch('SELECT * FROM cabinet_order WHERE out_wallet = $1', out_wallet)
         if result:
             return result[0]
         else:
@@ -276,7 +111,7 @@ async def update_order(order_id, **kwargs):
         var_number += 1
         vars.append(kwargs[key])
 
-    sql_query = "UPDATE bot_order SET %s WHERE id = $%d" % (sql_str[0:-1], var_number)
+    sql_query = "UPDATE cabinet_order SET %s WHERE id = $%d" % (sql_str[0:-1], var_number)
     vars.append(order_id)
 
     async with pool.acquire() as conn:
@@ -298,7 +133,7 @@ async def update_order(order_id, **kwargs):
 
 async def get_bot(bot_token):
     async with pool.acquire() as conn:
-        result = await conn.fetch('SELECT * FROM bot_bot '
+        result = await conn.fetch('SELECT * FROM cabinet_bot '
                                   'WHERE token = $1', bot_token)
 
         if result:
@@ -321,7 +156,7 @@ async def update_bot(bot_id, **kwargs):
         var_number += 1
         vars.append(kwargs[key])
 
-    sql_query = "UPDATE bot_bot SET %s WHERE id = $%d" % (sql_str[0:-1], var_number)
+    sql_query = "UPDATE cabinet_bot SET %s WHERE id = $%d" % (sql_str[0:-1], var_number)
     vars.append(bot_id)
 
     async with pool.acquire() as conn:
@@ -331,7 +166,7 @@ async def update_bot(bot_id, **kwargs):
 async def create_order(bot_id, user_id, order_text):
     async with pool.acquire() as conn:
 
-        query = "INSERT INTO bot_order " \
+        query = "INSERT INTO cabinet_order " \
                 "(bot_id, bot_user_id, text, date_in) " \
                 "VALUES " \
                 "($1, $2, $3, $4)"
@@ -342,7 +177,7 @@ async def create_order(bot_id, user_id, order_text):
 async def add_mes_log(bot_id, bot_user_id, in_or_out, log, answer_whatsapp_api=None):
     async with pool.acquire() as conn:
 
-        query = "INSERT INTO bot_messagelog " \
+        query = "INSERT INTO cabinet_messagelog " \
                 "(bot_id, bot_user_id, in_or_out, log, answer_whatsapp_api, date_in) " \
                 "VALUES " \
                 "($1, $2, $3, $4, $5, $6)"
